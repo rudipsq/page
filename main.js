96188944aa1b2b329f3c0da8b6de295bd4c5c58d
rudipsq <132471589+rudipsq@users.noreply.github.com>
2023-06-12 21:21:13 +0200

    Update main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// window.onload = function() {
-//     // similar behavior as clicking on a link
-//     window.location.href = "https://docs.google.com/presentation/d/1kjGFKsZye49eNsU0c0D48aNuFap1RjN-7fjU7DnEuok/edit?usp=sharing";
-// }
-
-window.onload = function() {
-    // similar behavior as an HTTP redirect
-    window.location.replace("https://docs.google.com/presentation/d/1kjGFKsZye49eNsU0c0D48aNuFap1RjN-7fjU7DnEuok/edit?usp=sharing");
-}
